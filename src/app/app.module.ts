ca71dd5073b4870038325d9e3fce2d364517ea47
BijanEisapour <bijanprogrammer@gmail.com>
2021-08-15 12:04:45 +0430

    [ADD] auth styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LandingComponent} from './pages/landing/landing.component';
-import {AuthComponent} from './pages/auth/auth.component';
 import {ShopComponent} from './pages/shop/shop.component';
 
 @NgModule({
-    declarations: [AppComponent, LandingComponent, AuthComponent, ShopComponent],
+    declarations: [AppComponent, LandingComponent, ShopComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
