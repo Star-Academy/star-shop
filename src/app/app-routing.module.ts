9f46a0094b2ed0ba8264fe8598b17a17b74b56b0
BijanEisapour <bijanprogrammer@gmail.com>
2021-08-15 11:23:29 +0430

    [PRETTIER] src

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LandingComponent} from './pages/landing/landing.component';
+import {AuthComponent} from './pages/auth/auth.component';
+import {ShopComponent} from './pages/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: LandingComponent},
+    {path: 'auth', component: AuthComponent},
+    {path: 'shop', component: ShopComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LandingComponent} from './pages/landing/landing.component';
+import {AuthComponent} from './pages/auth/auth.component';
+import {ShopComponent} from './pages/shop/shop.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, LandingComponent, AuthComponent, ShopComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
